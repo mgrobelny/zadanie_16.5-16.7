70dffbb9fdae2f7170b1f16aeae1d41325b6f919
mgrobelny <michalmaciejg@gmail.com>
2017-10-29 11:23:27 +0100

    dev-server added

--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,34 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var OptimizeJsPlugin = require('optimize-js-plugin');
 
+// pluginy dla środowiska developerskiego (ustawione domyślnie)
+var env = process.env.NODE_ENV || 'development';
+var plugins = [
+new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body',
+    })
+];
+
+console.log('NODE_ENV:', env);
+
+// pluginy dla środowiska produkcyjnego (uruchamiają się, jeśli napisać "NODE_ENV=production npm start")
+if (env === 'production') {
+plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeJsPlugin({
+      sourceMap: false
+    })
+  );
+}
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './build'),
     filename: 'app.bundle.js'
   },
-  plugins:
-  [new HtmlWebpackPlugin({
-    template: 'src/index.html',
-    filename: 'index.html',
-    inject: 'body'
-  }),
-  new webpack.optimize.UglifyJsPlugin(),
-  new OptimizeJsPlugin({
-    sourceMap: false
-  })
-  ],
-
   module: {
     rules: [
       {
